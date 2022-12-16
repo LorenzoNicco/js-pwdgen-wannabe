823e9bad9d5db55a126240f26fcacb81d20a7393
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-16 16:48:30 +0100

    Add generazione output in pagina/ prove 1,2 e 3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //richiesta nome
-const name = prompt('Inserisci il tuo nome');
+const firstname = prompt('Inserisci il tuo nome');
 
 //debug
 console.log ('Fin quì tutto ok');
 
 //richiesta cognome
-const surname = prompt('Inserisci il tuo cognome');
+const secondname = prompt('Inserisci il tuo cognome');
 
 //debug
 console.log ('Anche quì tutto ok');
@@ -19,6 +19,19 @@ console.log ('Ancora tutto a posto');
 //generazione output
 console.log(
     `
-    ${name}${surname}${color}'21'
+    ${firstname}${secondname}${color}21
     `
 );
+
+//generazione output in pagina prova1
+document.writeln(
+    `
+    ${firstname}${secondname}${color}21
+    `
+);
+
+//generazione output in pagina prova2
+document.writeln(firstname+secondname+color+21);
+
+//generazione output in pagina prova3
+document.writeln(`${firstname}${secondname}${color}21`);
