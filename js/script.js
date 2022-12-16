8cb95b97585692b519354405e3d4cb2799cb1aca
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-16 15:44:21 +0100

    Add richieste e output

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//richiesta nome
+const name = prompt('Inserisci il tuo nome');
+
+//debug
+console.log ('Fin quì tutto ok');
+
+//richiesta cognome
+const surname = prompt('Inserisci il tuo cognome');
+
+//debug
+console.log ('Anche quì tutto ok');
+
+//richiesta colore
+let color = prompt('Inserisci il tuo colore preferito');
+
+//debug
+console.log ('Ancora tutto a posto');
+
+//generazione output
+console.log(
+    `
+    ${name}${surname}${color}'21'
+    `
+);
